aeed0fdb2a67b189a1ce5c4a4efaa7341b35ba97
Lucas Magalhães <148398476+lucasmagalhaes021@users.noreply.github.com>
2025-07-07 17:15:43 -0300

    Fix S1075 - DAO - ignore constants used as default

--- src/main/java/com/dao/DAO.java
+++ src/main/java/com/dao/DAO.java
@@ -11,7 +11,6 @@ import java.util.logging.Level;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 
