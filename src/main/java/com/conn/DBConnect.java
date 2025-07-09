c46d47f2ed1c347c043e9c409202957d1943cbeb
Álamo <alamodepaula@gmail.com>
2025-07-09 10:49:37 -0300

    adição de testes de cobertura
    Adição de testes de cobertura para a entity Contactus

--- src/main/java/com/conn/DBConnect.java
+++ src/main/java/com/conn/DBConnect.java
@@ -14,7 +14,7 @@ public class DBConnect
 			
 			Class.forName("com.mysql.cj.jdbc.Driver");
 		//	conn = DriverManager.getConnection("jdbc:sqlite:mydatabase.db");
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecomerce","root","admin");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3308/ecomerce","root","adminroot@");
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
